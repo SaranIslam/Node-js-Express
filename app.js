3c0a523de975eedc2e97015fa96611972288d9d4
SaranIslam <saranisl.job@gmail.com>
2023-04-10 13:52:16 +0700

    add main

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const chalk = require('chalk');
+//const chalk = require('chalk');
 const debug = require('debug')('app');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(morgan('combined'));
 app.get("/",(req,res) =>{
 
-    res.send('Hello brooo');
+    res.send('Hello Brooo');
 })
 
 app.listen(port, ()=>{
-    debug("Listening on port"+ chalk.green(port));
+    debug("Listening on port"+port);
 })
